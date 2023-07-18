46556cbf9d582cf2903bc771202e2dd376868818
Eudis Alves <deleudis@gmail.com>
2023-07-17 21:30:39 -0300

    adicionando icones

--- TempCalc/src/components/AppNavigator/index.jsx
+++ TempCalc/src/components/AppNavigator/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import HomeScreen from '../HomeScren';
 import ProfileScreen from '../ProfileScreen';
 
@@ -8,10 +9,26 @@ import ProfileScreen from '../ProfileScreen';
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => (
-  <NavigationContainer>
+    <NavigationContainer>
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="user" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   </NavigationContainer>
 );
